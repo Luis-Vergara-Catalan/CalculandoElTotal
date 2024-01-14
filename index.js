ea278a9cfc2c8ffd63e7de2f030716e23f5463e6
luis-vergara-catalan <luis.vergara.catalan@gmail.com>
2024-01-14 16:40:02 -0300

    Calculando el total

---
+++ index.js
@@ -0,0 +1,8 @@
+precio = 400000
+
+precioSpan = document.querySelector(".precio-inicial"); //llamar a un elemento
+precioSpan.innerHTML = precio
+
+
+                
+//document.getElementById('precio-inicial') // otra forma de llamar un elemento
